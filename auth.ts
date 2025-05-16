4a9baef857711521eef05f8d431868aad7442984
Leo Kettmeir <crowlkats@toaxl.com>
2025-05-17 01:14:30 +0200

    feat: create app flow

--- auth.ts
+++ auth.ts
@@ -2,61 +2,79 @@ import type { TRPCCombinedDataTransformer } from "@trpc/server";
 import { serialize } from "superjson";
 import open from "open";
 import { encodeBase64 } from "@std/encoding";
-import { green } from "@std/fmt/colors";
+import { green, red } from "@std/fmt/colors";
 import {
   createTRPCClient,
   httpBatchStreamLink,
   httpSubscriptionLink,
   splitLink,
+  type TRPCClient,
 } from "@trpc/client";
 import { Spinner } from "@std/cli/unstable-spinner";
+import { promptSelect } from "@std/cli/unstable-prompt-select";
 
 export const deployUrl = Deno.env.get("DEPLOY_URL");
-export let deployToken = Deno.env.get("DEPLOY_TOKEN");
 
-const transformer: TRPCCombinedDataTransformer = {
-  input: {
-    serialize,
-    deserialize: (_) => {/* this is never called on the client */},
-  },
-  output: {
-    serialize: (_) => {/* this is never called on the client */},
-    deserialize: (object) => (0, eval)(`(${object})`),
-  },
-};
-export const trpcClient = createTRPCClient<any>({
-  links: [
-    splitLink({
-      // uses the httpSubscriptionLink for subscriptions
-      condition: (op) => op.type === "subscription",
-      false: httpBatchStreamLink({
-        url: deployUrl + "/api",
-        headers() {
-          if (deployToken) {
-            return {
-              cookie: `token=${deployToken}`,
-            };
-          } else {
-            return {};
-          }
-        },
-        transformer,
-      }),
-      true: httpSubscriptionLink({
-        url: deployUrl + "/api",
-        transformer,
-      }),
-    }),
-  ],
-});
+export function createTrpcClient(deployToken?: string) {
+  const transformer: TRPCCombinedDataTransformer = {
+    input: {
+      serialize,
+      deserialize: (_) => {/* this is never called on the client */},
+    },
+    output: {
+      serialize: (_) => {/* this is never called on the client */},
+      deserialize: (object) => (0, eval)(`(${object})`),
+    },
+  };
 
-if (!deployUrl) {
-  console.error(
-    "Expected the 'DEPLOY_URL' environmental variable to be specified.",
-  );
-  Deno.exit(1);
+  return createTRPCClient<any>({
+    links: [
+      splitLink({
+        // uses the httpSubscriptionLink for subscriptions
+        condition: (op) => op.type === "subscription",
+        false: httpBatchStreamLink({
+          url: deployUrl + "/api",
+          headers() {
+            if (deployToken) {
+              return {
+                cookie: `token=${deployToken}`,
+              };
+            } else {
+              return {};
+            }
+          },
+          transformer,
+        }),
+        true: httpSubscriptionLink({
+          url: deployUrl + "/api",
+          transformer,
+        }),
+      }),
+    ],
+  });
 }
-if (!deployToken) {
+
+export async function auth() {
+  let deployToken = Deno.env.get("DEPLOY_TOKEN");
+
+  if (!deployToken) {
+    const { code, exchangeToken, verifier } = await interactive();
+
+    const authUrl = `${deployUrl}/auth?code=${code}`;
+
+    console.log(`Visit ${authUrl} to authorize uploading of tarball.\x07`);
+    const spinner = new Spinner({ message: "Waiting...", color: "yellow" });
+    spinner.start();
+
+    await open(authUrl);
+
+    deployToken = await tokenExchange(exchangeToken, verifier, spinner);
+  }
+
+  return deployToken!;
+}
+
+export async function interactive(): Promise<{ code: string; exchangeToken: string; verifier: string; }> {
   const verifier = crypto.randomUUID();
   const data = (new TextEncoder()).encode(verifier);
   const hash = await crypto.subtle.digest("SHA-256", data);
@@ -74,20 +92,20 @@ if (!deployToken) {
 
   const body = await res.json();
 
-  const authUrl = `${deployUrl}/auth?code=${body.code}`;
+  return {
+    code: body.code,
+    exchangeToken: body.exchangeToken,
+    verifier,
+  }
+}
 
-  console.log(`Visit ${authUrl} to authorize uploading of tarball.\x07`);
-  const spinner = new Spinner({ message: "Waiting...", color: "yellow" });
-  spinner.start();
-
-  await open(authUrl);
-
-  deployToken = await new Promise((resolve, reject) => {
+export function tokenExchange(exchangeToken: string, verifier: string, spinner: Spinner): Promise<string> {
+  return new Promise((resolve, reject) => {
     const interval = setInterval(async () => {
       const res = await fetch(`${deployUrl}/auth/exchange`, {
         method: "POST",
         body: JSON.stringify({
-          exchangeToken: body.exchangeToken,
+          exchangeToken,
           verifier,
         }),
       });
@@ -106,7 +124,9 @@ if (!deployToken) {
         const err = await res.json();
         if (
           !(err.code === "AUTHORIZATION_PENDING" &&
-            err.message.endsWith("The requested authorization has not been approved or denied yet."))
+            err.message.endsWith(
+              "The requested authorization has not been approved or denied yet.",
+            ))
         ) {
           clearInterval(interval);
           reject(new Error(err.message));
@@ -115,3 +135,66 @@ if (!deployToken) {
     }, 2000);
   });
 }
+
+export async function withApp(
+  trpcClient: TRPCClient<any>,
+  org?: string,
+  app?: string,
+) {
+  if (!org || !app) {
+    const orgs: Array<{
+      name: string;
+      slug: string;
+      id: string;
+    }> = await trpcClient.orgs.list.query();
+
+    const orgStrings = orgs.map((org) => `${org.name} (${org.slug})`);
+    const orgsResult = promptSelect("select an organization:", orgStrings, {
+      clear: true,
+    });
+    if (!orgsResult) {
+      console.error("No organization was selected.");
+      Deno.exit(1);
+    }
+
+    const selectedOrg = orgs[orgStrings.indexOf(orgsResult)];
+    org = selectedOrg.slug;
+    console.log(`Selected organization '${selectedOrg.name}'`);
+
+    const apps: Array<{ name: string; slug: string }> = await trpcClient.apps
+      .list.query({
+        org: selectedOrg.id,
+      });
+    const appStrings = apps.map((app) => `${app.slug}`);
+    const appsResult = promptSelect("select an application:", appStrings, {
+      clear: true,
+    });
+    if (!appsResult) {
+      console.error("No organization was selected.");
+      Deno.exit(1);
+    }
+
+    const selectedApp = apps[appStrings.indexOf(appsResult)];
+    app = selectedApp.slug;
+    console.log(`Selected app '${selectedApp.slug}'`);
+  }
+
+  if (!org) {
+    console.error(
+      "Expected 'deploy.org' in the config file or the '--org' flag to be specified.",
+    );
+    Deno.exit(1);
+  }
+  if (!app) {
+    console.error(
+      "Expected 'deploy.app' in the config file or the '--app' flag to be specified.",
+    );
+    Deno.exit(1);
+  }
+
+  return {
+    org,
+    app,
+  };
+}
+
---
+++ publish.ts
@@ -0,0 +1,147 @@
+import { TarStream, type TarStreamDir, type TarStreamFile } from "@std/tar";
+import { compile as gitignoreCompile } from "@cfa/gitignore-parser";
+import { walk } from "@std/fs";
+import { ProgressBar } from "@std/cli/unstable-progress-bar";
+import { join, relative } from "@std/path";
+import { green, red, yellow } from "@std/fmt/colors";
+import { Config, writeConfig } from "./main.ts";
+import { deployUrl } from "./auth.ts";
+
+export async function publish(
+  rootPath: string,
+  configContent: Config | null,
+  deployToken: string,
+  org: string,
+  app: string,
+) {
+  let gitignore: any = {
+    denies: () => false,
+  };
+
+  try {
+    gitignore = gitignoreCompile(
+      Deno.readTextFileSync(join(rootPath, ".gitignore")),
+    );
+  } catch (_) {
+    //
+  }
+
+  const excludes = [/node_modules/, /.git/, /.DS_Store/];
+
+  const stream = ReadableStream.from(walk(rootPath, { skip: excludes }))
+    .pipeThrough(
+      new TransformStream({
+        transform(chunk, controller) {
+          const path = relative(rootPath, chunk.path);
+          const relativePath = join(
+            "source",
+            path + (chunk.isDirectory ? "/" : ""),
+          );
+          if (gitignore.denies(relativePath)) {
+            return;
+          }
+
+          controller.enqueue({ chunk, relativePath });
+        },
+      }),
+    );
+
+  const [counter, body] = stream.tee();
+
+  let total = 0;
+  for await (const { chunk } of counter) {
+    if (!chunk.isDirectory) {
+      total++;
+    }
+  }
+
+  const progress = new ProgressBar(Deno.stdout.writable, {
+    max: total,
+    emptyChar: " ",
+    fillChar: green("█"),
+    fmt(formatter) {
+      const minutes = (formatter.time / 1000 / 60 | 0).toString().padStart(
+        2,
+        "0",
+      );
+      const seconds = (formatter.time / 1000 % 60 | 0).toString().padStart(
+        2,
+        "0",
+      );
+
+      const length = formatter.max.toString().length;
+      return `[${yellow(minutes)}:${
+        yellow(seconds)
+      }] ${formatter.progressBar} ${
+        yellow(formatter.value.toString().padStart(length, " "))
+      }/${yellow(formatter.max.toString())} files uploaded.`;
+    },
+  });
+
+  const tarball = body
+    .pipeThrough(
+      new TransformStream({
+        async transform({ chunk, relativePath }, controller) {
+          if (chunk.isDirectory) {
+            controller.enqueue(
+              {
+                type: "directory",
+                path: relativePath,
+              } satisfies TarStreamDir,
+            );
+          } else {
+            const [stat, file] = await Promise.all([
+              Deno.stat(chunk.path),
+              Deno.open(chunk.path),
+            ]);
+
+            controller.enqueue(
+              {
+                type: "file",
+                path: relativePath,
+                size: stat.size,
+                readable: file.readable.pipeThrough(
+                  new TransformStream({
+                    flush() {
+                      progress.add(1);
+                    },
+                  }),
+                ),
+              } satisfies TarStreamFile,
+            );
+          }
+        },
+      }),
+    )
+    .pipeThrough(new TarStream())
+    .pipeThrough(new CompressionStream("gzip"));
+
+  const resp = await fetch(`${deployUrl}/api/trigger_tarball_build`, {
+    method: "POST",
+    headers: {
+      "x-meta": JSON.stringify({
+        org,
+        app,
+      }),
+      "cookie": `token=${deployToken}`,
+    },
+    body: tarball,
+  });
+
+  const resBody = await resp.json();
+
+  await progress.end();
+
+  if (!resp.ok) {
+    console.log();
+    console.log(`${red("✗")} An error occurred:`);
+    console.log(`  ${resBody.message}`);
+  } else {
+    console.log("Successfully uploaded tarball!");
+    console.log(
+      `You can view the revision here:\n${deployUrl}/${org}/${app}/builds/${resBody.revisionId}`,
+    );
+
+    await writeConfig(configContent, org, app);
+  }
+}
