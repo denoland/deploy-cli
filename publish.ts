71f2df3e25c6da30c6df4b1bc5ccefdaa5f35d39
Leo Kettmeir <crowlkats@toaxl.com>
2025-07-25 10:55:09 +0200

    feat: differential sync

--- publish.ts
+++ publish.ts
@@ -1,7 +1,8 @@
 import { TarStream, type TarStreamDir, type TarStreamFile } from "@std/tar";
 import { compile as gitignoreCompile } from "@cfa/gitignore-parser";
-import { walk } from "@std/fs";
+import { walk, type WalkEntry } from "@std/fs";
 import { ProgressBar } from "@std/cli/unstable-progress-bar";
+import { Spinner } from "@std/cli/unstable-spinner";
 import { join, relative, resolve } from "@std/path";
 import { green, yellow } from "@std/fmt/colors";
 import { type Config, writeConfig } from "./config.ts";
@@ -9,6 +10,13 @@ import { authedFetch } from "./auth.ts";
 import { error } from "./util.ts";
 
 const SEPARATOR_PATTERN = Deno.build.os === "windows" ? "\\\\" : "/";
+
+type Chunk =
+  & { chunk: WalkEntry; relativePath: string }
+  & ({ hash?: undefined; data?: undefined } | {
+    hash: string;
+    data: Uint8Array;
+  });
 
 export async function publish(
   deployUrl: string,
@@ -38,10 +46,12 @@ export async function publish(
 
   console.log(`Publishing '${resolve(rootPath)}'`);
 
-  const stream = ReadableStream.from(walk(rootPath, { skip: excludes }))
+  const stream: ReadableStream<Chunk> = ReadableStream.from(
+    walk(rootPath, { skip: excludes }),
+  )
     .pipeThrough(
       new TransformStream({
-        transform(chunk, controller) {
+        async transform(chunk, controller) {
           const path = relative(rootPath, chunk.path);
           const relativePath = join(
             "source",
@@ -51,111 +61,181 @@ export async function publish(
             return;
           }
 
-          controller.enqueue({ chunk, relativePath });
+          if (!chunk.isDirectory) {
+            const data = await Deno.readFile(chunk.path);
+
+            const hashBuffer = await crypto.subtle.digest("SHA-256", data!);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hash = hashArray.map((b) => b.toString(16).padStart(2, "0"))
+              .join("");
+
+            controller.enqueue({
+              chunk,
+              relativePath,
+              data,
+              hash,
+            });
+          } else {
+            controller.enqueue({
+              chunk,
+              relativePath,
+            });
+          }
         },
       }),
     );
 
   const [counter, body] = stream.tee();
 
+  const manifest: Record<string, string> = {};
   let total = 0;
-  for await (const { chunk } of counter) {
+
+  const hashesSpinner = new Spinner({
+    message: "Generating hashes...",
+  });
+  hashesSpinner.start();
+  for await (const { chunk, hash, relativePath } of counter) {
     if (!chunk.isDirectory) {
       total++;
+      const parts = relativePath.split("/");
+      parts.shift();
+      manifest[parts.join("/")] = hash!;
+    }
+  }
+  hashesSpinner.stop();
+  console.log(`${green("✔")} Generated hashes`);
+
+  const initiatedBuildRes = await authedFetch(deployUrl, "api/initiate_cli_build", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      org,
+      app,
+      production: prod,
+      manifest,
+    }),
+  });
+
+  const { revisionId }: { revisionId: string; } = await initiatedBuildRes.json();
+
+  let missingHashes: string[];
+
+  const s = Date.now();
+  while (true) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const maybeHashesRes = await authedFetch(deployUrl, `api/diffsync/${org}/${app}/${revisionId}`, {});
+    if (maybeHashesRes.status !== 202) {
+      if (maybeHashesRes.ok) {
+        missingHashes = await maybeHashesRes.json();
+        break;
+      } else {
+        const err = await maybeHashesRes.json();
+        error(`Failed getting file hashes: ${err.message}`, maybeHashesRes);
+      }
+    }
+
+    if ((Date.now() - s) >= 30 * 1000) {
+      error(`Failed getting file hashes`, maybeHashesRes);
     }
   }
 
-  const progress = new ProgressBar({
-    max: total,
-    emptyChar: " ",
-    fillChar: green("█"),
-    formatter(formatter) {
-      const minutes = (formatter.time / 1000 / 60 | 0).toString().padStart(
-        2,
-        "0",
-      );
-      const seconds = (formatter.time / 1000 % 60 | 0).toString().padStart(
-        2,
-        "0",
-      );
+  if (missingHashes.length > 0) {
+    const skippedFilesCount = total - missingHashes.length;
 
-      const length = formatter.max.toString().length;
-      return `[${yellow(minutes)}:${
-        yellow(seconds)
-      }] ${formatter.progressBar} ${
-        yellow(formatter.value.toString().padStart(length, " "))
-      }/${yellow(formatter.max.toString())} files uploaded.`;
-    },
-  });
+    if (skippedFilesCount > 0) {
+      console.log(`Found ${skippedFilesCount} already uploaded files, which will be skipped from uploading`);
+    }
 
-  const tarball = body
-    .pipeThrough(
-      new TransformStream({
-        async transform({ chunk, relativePath }, controller) {
-          if (chunk.isDirectory) {
-            controller.enqueue(
-              {
-                type: "directory",
-                path: relativePath,
-              } satisfies TarStreamDir,
-            );
-          } else {
-            const [stat, file] = await Promise.all([
-              Deno.stat(chunk.path),
-              Deno.open(chunk.path),
-            ]);
+    const progress = new ProgressBar({
+      max: missingHashes.length,
+      emptyChar: " ",
+      fillChar: green("█"),
+      formatter(formatter) {
+        const minutes = (formatter.time / 1000 / 60 | 0).toString().padStart(
+          2,
+          "0",
+        );
+        const seconds = (formatter.time / 1000 % 60 | 0).toString().padStart(
+          2,
+          "0",
+        );
 
-            controller.enqueue(
-              {
-                type: "file",
-                path: relativePath,
-                size: stat.size,
-                readable: file.readable.pipeThrough(
-                  new TransformStream({
-                    flush() {
-                      progress.value += 1;
-                    },
-                  }),
-                ),
-              } satisfies TarStreamFile,
-            );
-          }
-        },
-      }),
-    )
-    .pipeThrough(new TarStream())
-    .pipeThrough(new CompressionStream("gzip"));
+        const length = formatter.max.toString().length;
+        return `[${yellow(minutes)}:${
+          yellow(seconds)
+        }] ${formatter.progressBar} ${
+          yellow(formatter.value.toString().padStart(length, " "))
+        }/${yellow(formatter.max.toString())} files uploaded.`;
+      },
+    });
 
-  const resp = await authedFetch(deployUrl, "/api/trigger_tarball_build", {
-    method: "POST",
-    headers: {
-      "x-meta": JSON.stringify({
-        org,
-        app,
-        production: prod,
-      }),
-    },
-    body: tarball,
-  });
+    const tarball = body
+      .pipeThrough(
+        new TransformStream({
+          async transform({ chunk, relativePath, data, hash }, controller) {
+            if (chunk.isDirectory) {
+              controller.enqueue(
+                {
+                  type: "directory",
+                  path: relativePath,
+                } satisfies TarStreamDir,
+              );
+            } else if (missingHashes.includes(hash!)) {
+              const stat = await Deno.stat(chunk.path);
 
-  const resBody = await resp.json();
+              progress.value += 1;
 
-  await progress.stop();
+              controller.enqueue(
+                {
+                  type: "file",
+                  path: relativePath,
+                  size: stat.size,
+                  readable: ReadableStream.from([data!]),
+                } satisfies TarStreamFile,
+              );
+            }
+          },
+        }),
+      )
+      .pipeThrough(new TarStream())
+      .pipeThrough(new CompressionStream("gzip"));
 
-  console.log();
+    const resp = await authedFetch(deployUrl, `api/diffsync/${org}/${app}/${revisionId}`, {
+      method: "POST",
+      headers: {
+        "x-meta": JSON.stringify({
+          org,
+          app,
+          production: prod,
+        }),
+      },
+      body: tarball,
+    });
 
-  if (!resp.ok) {
-    error(resBody.message, resp);
-  } else {
+    await progress.stop();
+
+    console.log();
+
+    if (!resp.ok) {
+      const resBody = await resp.json();
+      error(resBody.message, resp);
+    }
+
     console.log("Successfully uploaded your application!");
-    console.log(
-      `You can view your application overview here:\n  ${deployUrl}/${org}/${app}`,
-    );
-    console.log(
-      `You can view the revision here:\n  ${deployUrl}/${org}/${app}/builds/${resBody.revisionId}`,
-    );
-    // TODO: print out the preview url
-
-    await writeConfig(configContent, rootPath, org, app);
+  } else {
+    console.log("No files were changed.");
   }
+
+  console.log(
+    `You can view your application overview here:\n  ${deployUrl}/${org}/${app}`,
+  );
+  console.log(
+    `You can view the revision here:\n  ${deployUrl}/${org}/${app}/builds/${revisionId}`,
+  );
+  // TODO: print out the preview url
+
+  await writeConfig(configContent, rootPath, org, app);
+
 }
