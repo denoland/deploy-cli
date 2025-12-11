3f7c2f20600eb84ae1b2bf9d7aee4ace00f75518
Yoshiya Hinosawa <stibium121@gmail.com>
2025-12-11 21:41:03 +0900

    feat: add `sandbox new` command (#25)

--- main.ts
+++ main.ts
@@ -19,6 +19,7 @@ import token_storage from "./token_storage.ts";
 import {
   sandboxKillCommand,
   sandboxListCommand,
+  sandboxNewCommand,
   sandboxSshCommand,
 } from "./sandbox.ts";
 
@@ -178,6 +179,7 @@ const sandboxCommand = new Command<GlobalOptions>()
   .action(() => {
     sandboxCommand.showHelp();
   })
+  .command("new", sandboxNewCommand)
   .command("list", sandboxListCommand)
   .command("kill", sandboxKillCommand)
   .command("ssh", sandboxSshCommand);
