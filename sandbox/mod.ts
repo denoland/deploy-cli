b9f802501199fb07b209493bece3de9619b10e3b
Leo Kettmeir <crowlkats@toaxl.com>
2026-01-15 10:44:10 +0100

    fix: validate endpoint

--- sandbox/mod.ts
+++ sandbox/mod.ts
@@ -688,6 +688,9 @@ export const sandboxCommand = new Command<GlobalOptions>()
     if (endpoint) {
       options.endpoint = endpoint;
     }
+    if (options.endpoint.endsWith("/")) {
+      error(false, "The provided DENO_DEPLOY_ENDPOINT is invalid.");
+    }
     const tokenEnv = options.token || Deno.env.get("DENO_DEPLOY_TOKEN");
     if (tokenEnv) {
       token_storage.set(tokenEnv, true);
