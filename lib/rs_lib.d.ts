fc6ce498db5b1937a3f51f919c79fdec46557c08
Uri <uri.valevski@gmail.com>
2026-04-12 05:37:51 +0300

    add pre-built WASM lib from @deno/deploy@0.0.94 for fork usage

---
+++ lib/rs_lib.d.ts
@@ -0,0 +1,9 @@
+// @generated file from wasmbuild -- do not edit
+// deno-lint-ignore-file
+// deno-fmt-ignore-file
+
+export function resolve_config(
+  root_path: string,
+  ignore_paths: string[],
+  allow_node_modules: boolean,
+): any;
